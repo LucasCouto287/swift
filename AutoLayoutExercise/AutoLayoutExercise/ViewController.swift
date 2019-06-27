a18889420237c1c024cd12227f3982e7ffb769ba
Abdullah Khan <abdullahkhan0503@gmail.com>
2019-06-26 19:13:44 -0600

    add AutoLayoutExercise
    learnt about Auto Layout using Storyboard (including size classes and varying traits)

---
+++ AutoLayoutExercise/AutoLayoutExercise/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AutoLayoutExercise
+//
+//  Created by Abdullah Khan on 2019-06-13.
+//  Copyright © 2019 Abdullah Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
+
